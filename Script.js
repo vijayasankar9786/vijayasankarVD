873ff19dc06d443adfe55cde56f78b246b5d0296
Vijayasankar <vijayasankar16052003@gmail.com>
2022-03-05 02:59:04 +0000

    first commit

---
+++ Script.js
@@ -0,0 +1,75 @@
+function Facebook() {
+    window.open("https://wwww.facebook.com/viji.shankar.1654")
+}
+
+function YouTube() {
+    window.open("https://youtube.com/channel/UCnShImWEdbCUkDa3uUEt-IA")
+}
+
+function Twitter() {
+    window.open("https://twitter.com/VijayasankarVD")
+}
+
+function LinkedIn() {
+    window.open("https://www.linkedin.com/jobs/")
+}
+
+function GitHub() {
+    window.open("https://github.com/vijayasankar9786")
+}
+
+function About() {
+    var x1 = document.getElementById('AboutPage');
+    if (x1.style.display === "block") {
+        x1.style.display = "none";
+    } else {
+        x1.style.display = "block";
+    }
+}
+
+function Resume() {
+    var x2 = document.getElementById('ResumePage');
+    if (x2.style.display === "block") {
+        x2.style.display = "none";
+    } else {
+        x2.style.display = "block";
+    }
+}
+
+function Contact() {
+    var C1 = document.getElementById('ContactPage');
+    if (C1.style.display === "block") {
+        C1.style.display = "none";
+    } else {
+        C1.style.display = "block";
+    }
+}
+
+function PageLeftAbout() {
+    var x3 = document.getElementById('AboutPage')
+    x3.style.display = "none"
+}
+
+function PageLeftResume() {
+    var x3 = document.getElementById('ResumePage')
+    x3.style.display = "none"
+}
+
+function PageLeftContact() {
+    var x3 = document.getElementById('ContactPage')
+    x3.style.display = "none"
+}
+
+function HireMe() {
+    var x4 = document.getElementById('HirePage')
+    x4.style.display = "block"
+}
+
+function PageLeftInResume() {
+    var x4 = document.getElementById('HirePage')
+    x4.style.display = "none"
+}
+
+function Mail() {
+    window.open("https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRlQzBRxGpPXdlpsSfcXzVmPfrdGZSjPhVNBvJTtmHCtXgrBvHRfXfXgTprMdfWfVKtVdcG");
+}
